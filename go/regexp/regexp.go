5a0de969587997a58765f1c1af1664b514277af9
s-tajima <tajima1989@gmail.com>
2016-09-18 11:45:51 +0900

    fix

---
+++ go/regexp/regexp.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"html"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	keywords, _ := ioutil.ReadFile("./keywords.txt")
+
+	for n := 1; n < 7001; n++ {
+		content, err := ioutil.ReadFile("./descriptions/" + strconv.Itoa(n) + ".txt")
+		panicIf(err)
+
+		result := Htmlify(string(content), strings.Split(strings.TrimSpace(string(keywords)), "\n"))
+		err = ioutil.WriteFile("./descriptions_html/"+strconv.Itoa(n)+".txt", []byte(result), os.ModePerm)
+		panicIf(err)
+	}
+}
+
+func Htmlify(content string, keywords []string) string {
+	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+	kw2sha := make(map[string]string)
+	content = re.ReplaceAllStringFunc(content, func(kw string) string {
+		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+		return kw2sha[kw]
+	})
+	content = html.EscapeString(content)
+	for kw, hash := range kw2sha {
+		u, err := url.Parse("http://localhost/keyword/" + pathURIEscape(kw))
+		panicIf(err)
+		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
+		content = strings.Replace(content, hash, link, -1)
+	}
+	return strings.Replace(content, "\n", "<br />\n", -1)
+}
+
+func Htmlify2(content string, re *regexp.Regexp) string {
+	kw2sha := make(map[string]string)
+	content = re.ReplaceAllStringFunc(content, func(kw string) string {
+		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+		return kw2sha[kw]
+	})
+	content = html.EscapeString(content)
+	for kw, hash := range kw2sha {
+		u, err := url.Parse("http://localhost/keyword/" + pathURIEscape(kw))
+		panicIf(err)
+		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
+		content = strings.Replace(content, hash, link, -1)
+	}
+	return strings.Replace(content, "\n", "<br />\n", -1)
+}
+
+func pathURIEscape(s string) string {
+	return (&url.URL{Path: s}).String()
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go/regexp/regexp_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestHtmlify(t *testing.T) {
+	f, _ := ioutil.ReadFile("./keywords.txt")
+	keywords := strings.Split(strings.TrimSpace((string(f))), "\n")
+	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+
+	for n := 1; n < 10; n++ {
+		t.Logf("Test start for %d.txt", n)
+		desc, err := ioutil.ReadFile("./descriptions/" + strconv.Itoa((n)) + ".txt")
+		panicIf(err)
+
+		expected, err := ioutil.ReadFile("./descriptions_html/" + strconv.Itoa((n)) + ".txt")
+		panicIf(err)
+
+		actual := Htmlify(string(desc), keywords)
+		if actual != string(expected) {
+			t.Errorf("Htmlify failed: %d", f)
+		}
+
+		actual = Htmlify2(string(desc), re)
+		if actual != string(expected) {
+			t.Errorf("Htmlify2 failed: %d", f)
+		}
+	}
+}
+
+func BenchmarkHtmlify(b *testing.B) {
+	f, _ := ioutil.ReadFile("./keywords.txt")
+	keywords := strings.Split(strings.TrimSpace((string(f))), "\n")
+
+	for n := 1; n < 10; n++ {
+		b.Logf("Test start for %d.txt", n)
+		desc, err := ioutil.ReadFile("./descriptions/" + strconv.Itoa((n)) + ".txt")
+		panicIf(err)
+
+		_ = Htmlify(string(desc), keywords)
+	}
+}
+
+func BenchmarkHtmlify2(b *testing.B) {
+	f, _ := ioutil.ReadFile("./keywords.txt")
+	keywords := strings.Split(strings.TrimSpace((string(f))), "\n")
+	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+
+	for n := 1; n < 10; n++ {
+		b.Logf("Test start for %d.txt", n)
+		desc, err := ioutil.ReadFile("./descriptions/" + strconv.Itoa((n)) + ".txt")
+		panicIf(err)
+
+		_ = Htmlify2(string(desc), re)
+	}
+}
